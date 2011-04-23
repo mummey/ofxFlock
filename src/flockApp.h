3c986186b66d228793003cc49be973fd7f69e68a
Brian Walsh <mummey@mac.com>
2011-04-23 16:28:27 -0700

    added files from empty example.
    Note: project must be placed in (OF-root)/apps/(app group name)/macFlock for the project to build. This restriction appears to be typical for OF projects built in XCode (header and library paths appear to be relative, the alternative could be a parent project managing everything OF-related, or defining environment variables set by a script during install).

---
+++ src/flockApp.h
@@ -0,0 +1,25 @@
+#ifndef _FLOCK_APP
+#define _FLOCK_APP
+
+
+#include "ofMain.h"
+
+class flockApp : public ofBaseApp
+{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed  (int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+
+};
+
+#endif
